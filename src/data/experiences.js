87d66da81b04c22268ca7796f3587665558b6e6d
Alejandro Cortell Marín <alejandrocortellmarin@gmail.com>
2024-03-03 12:50:30 +0100

    Astro webpage

---
+++ src/data/experiences.js
@@ -0,0 +1,26 @@
+export const experiences = [
+  {
+    img: '/assets/img/Touchpix.png',
+    'alt-img': 'Touchpix',
+    time: 'touchpix-time',
+    description: 'touchpix-description',
+  },
+  {
+    img: '/assets/img/Tecdam.svg',
+    'alt-img': 'Tecdam',
+    time: 'tecdam-time',
+    description: 'tecdam-description',
+  },
+  {
+    img: '/assets/img/Balterra.svg',
+    'alt-img': 'Balterra',
+    time: 'balterra-time',
+    description: 'balterra-description',
+  },
+  {
+    img: '/assets/img/flexografico.svg',
+    'alt-img': 'Flexografico',
+    time: 'flexo-time',
+    description: 'flexo-description',
+  },
+]
---
+++ src/data/knoweledge.js
@@ -0,0 +1,62 @@
+export const knowledge = [
+  [
+    {
+      name: 'react',
+      link: 'https://reactjs.org/',
+    },
+    {
+      name: 'react-native',
+      link: 'https://reactnative.dev/',
+    },
+    {
+      name: 'axios',
+      link: 'https://axios-http.com/',
+    },
+    {
+      name: 'redux',
+      link: 'https://redux.js.org/',
+    },
+    {
+      name: 'i18n',
+      link: 'https://www.npmjs.com/package/i18n',
+    },
+    {
+      name: 'react-router',
+      link: 'https://reactrouter.com/en/main',
+    },
+    {
+      name: 'ffmpeg',
+      link: 'https://ffmpeg.org/',
+    },
+  ],
+  [
+    {
+      name: 'nextjs',
+      link: 'https://nextjs.org/',
+    },
+    {
+      name: 'typescript',
+      link: 'https://www.typescriptlang.org/',
+    },
+    {
+      name: 'sass',
+      link: 'https://sass-lang.com/',
+    },
+    {
+      name: 'cypress',
+      link: 'https://www.cypress.io/',
+    },
+    {
+      name: 'testing-library',
+      link: 'https://testing-library.com/',
+    },
+    {
+      name: 'express',
+      link: 'https://expressjs.com',
+    },
+    {
+      name: 'multer',
+      link: 'https://www.npmjs.com/package/multer',
+    },
+  ],
+]
